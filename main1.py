0433b31fb921805b65ccd3db968f4f9faf6d9084
Paradox <123651510+1808goya@users.noreply.github.com>
2025-05-31 13:54:42 +0900

    김정기 1 투사체 class 구현(projectile.py) 투사체 종류 분할(projectile_types.py) main 함수에 예시로 투사체 발사 추가 assets에 투사체 spritesheet 추가

--- main1.py
+++ main1.py
@@ -4,8 +4,11 @@ from start_screen import StartScreen
 from ui import GameUI
 from sprites.player import Player
 from sprites.npc import NPC
+from sprites.projectile import Projectile, load_projectile_sprites
+from sprites.projectile_types import PROJECTILE_TYPES
 
 def main():
+    
     """메인 게임 함수"""
     # Pygame 초기화
     pygame.init()
@@ -34,6 +37,19 @@ def main():
     player = Player(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
     npc = NPC(SCREEN_WIDTH//2 + 100, SCREEN_HEIGHT//2)
     
+    
+    # 투사체 생성
+    projectile_sprites = load_projectile_sprites("assets/images/projectiles.png")
+
+    # 투사체 그룹 생성
+    projectiles = pygame.sprite.Group()
+    
+    # (임시) 기본 투사체 설정(projectile_types 참조)
+    projectile_type = PROJECTILE_TYPES["yellow"] # 투사체 색(특성) 가져오기
+    projectile_timer = 0
+    projectile_cooldown = projectile_type["cooldown"] # 투사체 발사간격 가져오기
+    
+    
     # 게임 루프
     running = True
     clock = pygame.time.Clock()
@@ -49,6 +65,37 @@ def main():
         player.update(keys)
         npc.update()
         
+        # 투사체 쿨다운 타이머 증가
+        projectile_timer += clock.get_time()
+        
+        # 쿨타임을 넘어서면 0으로 타이머 리셋시킴
+        if projectile_timer >= projectile_cooldown:
+            projectile_timer = 0  # 타이머 리셋
+
+       # 현재 projectile_type에서 속성 추출
+            index = projectile_type["index"]
+            speed = projectile_type["speed"]
+            damage = projectile_type["damage"]
+
+            # type에 맞는 속성 적용한 투사체 만들기
+            proj = Projectile( 
+                # 플레이어 위치와 NPC 위치를 참조(둘 다 중앙에서 시작)하여 속도를 정함
+                npc.rect.centerx, npc.rect.centery,
+                player.rect.centerx, player.rect.centery,
+                projectile_sprites[index],
+                speed=speed,
+                damage=damage
+        )
+            # 투사체 group에 추가(화면 벗어나면 삭제됨)
+            projectiles.add(proj)
+
+        # 투사체 위치 업뎃
+        projectiles.update(clock.get_time())
+
+        
+        
+        
+        
         # UI 업데이트 (예시 값 사용)
         game_ui.update(
             hp=player.hp,  # 실제 HP 값 연결
@@ -58,10 +105,13 @@ def main():
         
         # 화면 그리기
         screen.fill((30, 30, 30))
+    
         
         # 스프라이트 그리기
         screen.blit(player.image, player.rect)
         screen.blit(npc.image, npc.rect)
+        
+        projectiles.draw(screen)
         
         # UI 그리기
         game_ui.draw(screen)
